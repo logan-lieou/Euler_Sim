8f62a6a901b2a38c115fa138cb6ec3f9d9b572a3
logan-lieou <logan.lieou@protonmail.com>
2021-04-10 01:44:54 -0500

    failing

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Graphics/RectangleShape.hpp>
 #include <iostream>
-#define SCALE 5
-#include "physics.h"
+#include <vector>
+#include "physics.hpp"
+#define SCALE 8
 
+// WIP
 void Render(sf::RenderWindow &win, int size) {
     win.clear();
     for (int i = 0; i < size; i++) {
@@ -10,25 +13,32 @@ void Render(sf::RenderWindow &win, int size) {
             sf::RectangleShape rect;
             rect.setSize(sf::Vector2f(SCALE, SCALE));
             rect.setPosition(j * SCALE, i * SCALE);
-            rect.setFillColor(sf::Color::Red);
+            rect.setFillColor(sf::Color(255, 255, 255, 255));
             win.draw(rect);
         }
     }
 }
 
+sf::RectangleShape SpawnParticle(sf::RenderWindow &window, sf::Vector2f pos, sf::Time t)
+{
+  sf::RectangleShape rect; // init rect
+  rect.setSize(sf::Vector2f(8.f, 8.f)); // 8x8 px's
+  rect.setPosition(pos); // update position
+  rect.setFillColor(sf::Color::White); // fill color is white
+  gravity(rect, t, pos);
+  window.draw(rect); // draw rectangle to the window
+  return rect;
+}
+
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(800, 600), "Euler Fluid Simulation");
-	const sf::Vector2f &size = sf::Vector2f(5.f, 5.f);
-	sf::RectangleShape shape(size);
-	shape.setFillColor(sf::Color::White);
+  sf::RenderWindow window(sf::VideoMode(800, 600), "Euler Fluid Simulation");
+  sf::Clock clock; sf::Time t;
+  std::vector<sf::RectangleShape> rects;
+  std::vector<sf::RectangleShape>::iterator i = rects.begin();
 
-    sf::Clock clock;
-    sf::Time delta_t;
-    float y;
-
-	while(window.isOpen())
-	{
+  while(window.isOpen())
+  {
 		sf::Event event;
 
 		// handling events
@@ -38,15 +48,20 @@ int main()
 				window.close();
 		}
 
-		// for physics
-        delta_t = clock.getElapsedTime();
+    t = clock.getElapsedTime();
 
-        Render(window, 50);
+		// if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+			// shape.setPosition(static_cast<sf::Vector2f>(sf::Mouse::getPosition(window)));
 
-		if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) 
-			shape.setPosition(static_cast<sf::Vector2f>(sf::Mouse::getPosition(window)));
-		window.clear();
-		window.draw(shape);
+	  if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+	    rects.insert(i, SpawnParticle(window, static_cast<sf::Vector2f>(sf::Mouse::getPosition(window)), t));
+      i += 1;
+    
+    for (int j = 0; j < rects.size(); j++)
+    {
+      gravity(rects.at(j), t, rects.at(j).getPosition());
+    }
+
 		window.display();
 	}
 }
--- src/physics.cpp
+++ src/physics.cpp
@@ -1,11 +1,20 @@
 //
 // Created by loganj on 4/9/21.
 //
-#include "physics.h"
+#include "physics.hpp"
 
-void gravity(sf::RectangleShape &rect, sf::Time t, float x)
+void gravity(sf::RectangleShape &rect, sf::Time t, sf::Vector2f pos)
 {
-    // gravity occurs along the y-axis
-    auto new_pos = static_cast<float>((0.5)*(9.8)*t.asSeconds());
-    rect.setPosition(x, new_pos);
+  auto new_pos = pos.y += static_cast<float>((0.5)*(9.8)*t.asSeconds());
+  rect.setPosition(pos.x, new_pos);
+}
+
+int IX(int x, int y, int N)
+{
+  (x < 0) ? x=0 : x=x;
+  (x > N-1) ? x=N-1 : x=x;
+  (y < 0) ? y=0 : y=y;
+  (y > N-1) ? y=N-1 : y=y;
+
+  return (y*N) + x;
 }
